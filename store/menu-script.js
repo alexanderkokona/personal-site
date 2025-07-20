152c152cf346e4eeb3d1d63271c776c36d08c520
Alexander Kokona <157238163+alexanderkokona@users.noreply.github.com>
2025-07-19 22:37:38 -0600

    menu?

--- store/menu-script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.querySelector(".menu-button");
-    const menu = document.getElementById("menu");
-
-    menuButton.addEventListener("click", () => {
-        menu.classList.toggle("open");
-    });
-});
